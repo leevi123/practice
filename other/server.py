b1f124cd6e7239eb55ed2146181d2aa2b03edb4f
leevi123 <leevi.laitinen22@gmail.com>
2025-02-19 22:33:43 +0200

    yes

--- other/server.py
+++ other/server.py
@@ -2,6 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
+
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.get_json()
